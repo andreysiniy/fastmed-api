037a7144647d7fe15ba7a998cfe9cd7698857632
andreysiniy <kakavvmm@gmail.com>
2025-04-15 22:54:52 +0300

    Added context class

---
+++ fastmed-api/Data/FastmedContext.cs
@@ -0,0 +1,17 @@
+﻿using fastmed_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace fastmed_api.Data;
+
+public class FastmedContext : DbContext
+{
+    public FastmedContext(DbContextOptions<FastmedContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+    
+    public DbSet<ClinicCard> ClinicCards { get; set; }
+    public DbSet<DoctorCard> DoctorCards { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<WorkingHour> WorkingHours { get; set; }
+}
