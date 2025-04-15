1b8dcbc69a305eb056083666d302435a4742d571
andreysiniy <kakavvmm@gmail.com>
2025-04-15 23:21:44 +0300

    Added DoctorRepository with basic methods

---
+++ fastmed-api/Repositories/DoctorRepository.cs
@@ -0,0 +1,52 @@
+﻿using fastmed_api.Data;
+using fastmed_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace fastmed_api.Repositories;
+
+public class DoctorRepository : IDoctorRepository
+{
+    private readonly FastmedContext _context;
+
+    public DoctorRepository(FastmedContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<DoctorCard>> GetDoctorCardsAsync()
+    {
+        return await _context.DoctorCards.ToListAsync();
+    }
+
+    public async Task<DoctorCard> GetDoctorCardAsync(int id)
+    {
+        return await _context.DoctorCards.FindAsync(id) ?? throw new NullReferenceException();
+    }
+    
+    public async Task AddDoctorCardAsync(DoctorCard doctorCard)
+    {
+        await _context.DoctorCards.AddAsync(doctorCard);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateDoctorCardAsync(DoctorCard doctorCard)
+    {
+        _context.DoctorCards.Update(doctorCard);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteDoctorCardAsync(int dococtorCardId)
+    {
+        var doctorCard = await _context.DoctorCards.FindAsync(dococtorCardId);
+        if (doctorCard != null)
+            _context.DoctorCards.Remove(doctorCard);
+        else
+            throw new NullReferenceException();
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<ClinicCard>> GetDoctorCardsByClinicIdAsync(int clinicId)
+    {
+        return await _context.ClinicCards.Where(c => c.ClinicId == clinicId).ToListAsync();
+    }
+}
---
+++ fastmed-api/Repositories/IDoctorRepository.cs
@@ -0,0 +1,13 @@
+﻿using fastmed_api.Models;
+
+namespace fastmed_api.Repositories;
+
+public interface IDoctorRepository
+{
+    Task<IEnumerable<DoctorCard>> GetDoctorCardsAsync();
+    Task<DoctorCard> GetDoctorCardAsync(int id);
+    Task AddDoctorCardAsync(DoctorCard doctorCard);
+    Task UpdateDoctorCardAsync(DoctorCard doctorCard);
+    Task DeleteDoctorCardAsync(int doctorCardId);
+    Task<IEnumerable<ClinicCard>> GetDoctorCardsByClinicIdAsync(int clinicId);
+}
