fbca5b061901e213ff721fe6a5764166da6d4077
andreysiniy <kakavvmm@gmail.com>
2025-04-15 23:01:13 +0300

    Splitted ClinicCard to have WorkingHour as a different model

--- fastmed-api/Models/ClinicCard.cs
+++ fastmed-api/Models/ClinicCard.cs
@@ -16,14 +16,3 @@ public class ClinicCard
     public ICollection<DoctorCard> Doctors { get; set; } = new List<DoctorCard>();
 }
 
-[Table("WorkingHours")]
-public class WorkingHour
-{
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    [Column("id")] public int Id { get; set; }
-    [Column("clinic_id")] public int ClinicId { get; set; }
-    [ForeignKey("ClinicId")] public ClinicCard Clinic { get; set; } = null!;
-    [Column("day_of_week")] public int DayOfWeek { get; set; } // 0 = Sunday, 1 = Monday, ...
-    [Column("open_time")] public TimeSpan OpenTime { get; set; }
-    [Column("close_time")] public TimeSpan CloseTime { get; set; }
-}
---
+++ fastmed-api/Models/WorkingHour.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fastmed_api.Models;
+
+[Table("WorkingHours")]
+public class WorkingHour
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")] public int Id { get; set; }
+    [Column("clinic_id")] public int ClinicId { get; set; }
+    [ForeignKey("ClinicId")] public ClinicCard Clinic { get; set; } = null!;
+    [Column("day_of_week")] public int DayOfWeek { get; set; } // 0 = Sunday, 1 = Monday, ...
+    [Column("open_time")] public TimeSpan OpenTime { get; set; }
+    [Column("close_time")] public TimeSpan CloseTime { get; set; }
+}
